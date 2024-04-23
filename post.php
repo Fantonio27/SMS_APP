088d53933f4b3516d0fff63c5f0d50eb907ae9b7
Francis Louie Antonio <francis.antonio0927@gmail.com>
2024-04-23 21:38:39 +0800

    Initial

--- post.php
+++ post.php
@@ -2,8 +2,6 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// $sid = "AC331dbe79a70bdbc1c012a924ed259a8d";
-// $sid = "AC331dbe79a70bdbc1c012a924ed259a8d";
 $token = "";
 $token = "";
 $client = new Twilio\Rest\Client($sid, $token);
