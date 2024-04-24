ffbdc30e2137d4dcdfe03f0d8c497e08b890dc35
Francis Louie Antonio <francis.antonio0927@gmail.com>
2024-04-24 11:21:41 +0800

    hide the sid and token

--- post.php
+++ post.php
@@ -2,25 +2,20 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// $token = "";
-// $token = "";
-// $client = new Twilio\Rest\Client($sid, $token);
-
-$client = new Twilio\Rest\Client("", "");
+$sid = "";
+$token = "";
+$client = new Twilio\Rest\Client($sid, $token);
 
 $number = $_POST['number']; 
 $message = $_POST['message'];
 
-if(!empty($number) and !empty($message)){
+if(!empty($number) and !empty($message))
     $client->messages->create(
         strval('+63'.$number),
         [
             'from' => '+19492817431',
-            'body' => 'Message Testing 123'
+            'body' => $message
         ]
     );
-
     echo "SMS Message Sent";
-}
-
-
+?>
